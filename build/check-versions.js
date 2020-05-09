be6ff5cb3e984dfd4e2626a550928df36c49b95c
Georgios Katsanos <georgios.katsanos@here.com>
2020-05-09 21:57:48 +0200

    Fixing package file

--- build/check-versions.js
+++ build/check-versions.js
@@ -15,14 +15,6 @@ const versionRequirements = [
   }
 ]
 
-if (shell.which('npm')) {
-  versionRequirements.push({
-    name: 'npm',
-    currentVersion: exec('npm --version'),
-    versionRequirement: packageConfig.engines.npm
-  })
-}
-
 module.exports = function () {
   const warnings = []
   for (let i = 0; i < versionRequirements.length; i++) {
